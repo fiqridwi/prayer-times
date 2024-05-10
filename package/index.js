a54599b193489cbc518b7d62e58ab97696c89664
Fiqri Dwi <nurfiqri23@gmail.com>
2024-05-10 21:36:58 +0700

    makes latlong params

--- package/index.js
+++ package/index.js
@@ -1,23 +1,22 @@
-async function getPrayer() {
+async function getPrayer(latitude, longitude) {
 	const BASE_URL = 'https://api.aladhan.com/v1/calendar';
 	const date = new Date();
 	const year = date.getFullYear();
 	const month = date.getMonth() + 1;
-	const latitude = -6.2088;
-	const longitude = 106.8456;
 	const rawResponse = await fetch(
 		`${BASE_URL}/${year}/${month}?method=15&shafaq=general&latitude=${latitude}&longitude=${longitude}`
 	);
 	const response = await rawResponse.json();
-	const todayPrayer = response.data.find((time) => {
-		return (
-			time.date.gregorian.date ===
-			new Date()
-				.toLocaleDateString('id-ID', { month: '2-digit', day: '2-digit', year: 'numeric' })
-				.replace(new RegExp(/\//g), '-')
-		);
-	});
-	return todayPrayer;
+	const currentDate = new Date()
+		.toLocaleDateString('id-ID', { month: '2-digit', day: '2-digit', year: 'numeric' })
+		.replace(new RegExp(/\//g), '-');
+
+	const data =
+		response.code !== 200
+			? response.data
+			: response.data.find((time) => time.date.gregorian.date === currentDate);
+
+	return data;
 }
 
 module.exports = getPrayer;
--- test-package/script.js
+++ test-package/script.js
@@ -1,7 +1,10 @@
 const getPrayer = require('prayer-times');
 
 const get = async () => {
-	const prayer = await getPrayer();
+	const latitude = -6.21462;
+	const longitude = 0;
+
+	const prayer = await getPrayer(latitude, longitude);
 
 	console.log(prayer);
 };
